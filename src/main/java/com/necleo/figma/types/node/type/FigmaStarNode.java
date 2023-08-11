74394bbfcdff89a5ec2788abe58fa1fefddba743
Riktam Santra <riktamsantra56@gmail.com>
2023-08-11 17:53:35 +0530

    added corner and fill stroke properties for FigmaStarNode

--- src/main/java/com/necleo/figma/types/node/type/FigmaStarNode.java
+++ src/main/java/com/necleo/figma/types/node/type/FigmaStarNode.java
@@ -1,6 +1,9 @@
 package com.necleo.figma.types.node.type;
 
+import com.fasterxml.jackson.annotation.JsonUnwrapped;
 import com.necleo.figma.types.FigmaNode;
+import com.necleo.figma.types.node.props.CornerProps;
+import com.necleo.figma.types.node.props.FillStrokeProps;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.experimental.FieldDefaults;
@@ -11,7 +14,13 @@ import lombok.extern.jackson.Jacksonized;
 @FieldDefaults(level = AccessLevel.PRIVATE)
 @SuperBuilder
 @Jacksonized
-public class FigmaStarNode extends FigmaNode {
+public class FigmaStarNode extends FigmaNode implements Corner, FillStroke{
     int pointCount;
     int innerRadius;
+
+    @JsonUnwrapped
+    CornerProps corner;
+
+    @JsonUnwrapped
+    FillStrokeProps fillStroke;
 }
